217629efffe123dbfa3dbcf81b6352245680c3b6
Kilna <kilna@kilna.com>
2018-04-28 10:59:45 -0700

    Initial development

--- prompt.sh
+++ prompt.sh
@@ -11,32 +11,35 @@ EOF
   return 1
 fi
 
-set_title() { export PS1_TITLE="$1"; }
-unset_title() { unset PS1_TITLE; }
-
-set_prompt() {
-  if [[ "$PS1_ORIG" == "" ]]; then
-    export PS1_ORIG="$PS1"
-  fi
-  export PS1="$(__ps1_prompt "$@" :title)";
+set_title() {
+  export PS1_TITLE="$1"
 }
 
-default_prompt() {
-  set_prompt :statusline :user '@' :host ' ' :dir :eol :prompt ' ' :reset
-}
-
-unset_prompt() {
-  export PS1="$PS1_ORIG"
-  unset PS1_ORIG
+unset_title() {
+  unset PS1_TITLE
 }
 
 reset_prompt() {
-  unset_prompt
+  #env | grep PS1_ | __ps1_debug 
+  export PS1="$PS1_ORIG"
+  unset PS1_ORIG
   unset PS1_MODULES
-  unset PS1_TYPE
+  unset PS1_STYLES
+  unset PS1_DEBUG
+  unset PS1_TITLE
+  unset PS1_TITLE_FORMAT
+  #env | grep PS1_ | __ps1_debug 
+  for func in "$(typeset -F | cut -d ' ' -f 3 | grep -E '^__ps1_')"; do
+    unset -f $func
+  done
   source $PS1_ROOT/prompt.sh "$@"
 }
 
+__ps1_debug() {
+  (( "${PS1_DEBUG:-0}" )) || return
+  echo "$@" >&2
+  #cat - >&2
+}
 
 __ps1_unicode() {
   case "$LANG" in
@@ -88,7 +91,7 @@ __ps1_ansi_echo() {
 __ps1_stylize() {
   content="$1"
   shift
-  __ps1_ansi $(__ps1_style "$@") "$content" :reset
+  __ps1_ansi $(__ps1_style "$@") "$content"
 }
 
 __ps1_ansi() {
@@ -101,6 +104,7 @@ __ps1_ansi() {
       :eol)    __ps1_ansi_echo '\e[0m\n\e[0m';; # Handy when bash eats a trailing newline
       :clear)  __ps1_ansi_echo '\e[H\e[2J';;
       :reset)  __ps1_ansi_echo '\e[0m';;
+      :style)  __ps1_ansi_echo $(__ps1_style "$1"); shift;;
       :fg|:bg) __ps1_ansi_echo $(__ps1_color "$arg" "$1"); shift;;
       +*|-*)   __ps1_ansi_echo $(__ps1_mode "$arg");;
       *)       echo -n "$arg";;
@@ -111,7 +115,7 @@ __ps1_ansi() {
 __ps1_prompt_stylize() {
   content="$1"
   shift
-  __ps1_prompt $(__ps1_style "$@") "$content" :reset
+  __ps1_prompt $(__ps1_style "$@") "$content" 
 }
 
 __ps1_prompt() {
@@ -119,15 +123,12 @@ __ps1_prompt() {
     arg="$1"
     shift
     case "$arg" in
-      :bootstrap)     
-        echo -n '`__ps1_bootstrap`';;
-      :title)     
-        local title="${PS1_TITLE_FORMAT:-\\z\\-\\u@\\h \\w}"
-        title="${title//\\z/'`echo -n "$PS1_TITLE"`'}"
-        title="${title//\\-/'`echo -n "${PS1_TITLE:+ - }"`'}"
-        str+='\[\e]0;\]'"$title"'\a'
-        echo -n "${str//\`\`/;}"
-        ;;
+      :title)         if [[ "${PS1_NO_TITLE:-0}" != 0 ]]; then
+                        local title="${PS1_TITLE_FORMAT:-\\z\\-\\u@\\h \\w}"
+                        title="${title//\\z/'`echo -n "$PS1_TITLE"`'}"
+                        title="${title//\\-/'`echo -n "${PS1_TITLE:+ - }"`'}"
+                        echo -n '\[\e]0;\]'"${title//\`\`/;}"'\a'
+                      fi;;
       :nl|:newline)   echo -n '\n';;
       :space)         echo -n ' ';;
       :eol)           echo -n '\[\e[0m\]\n\[\e[0m\]';;
@@ -154,6 +155,7 @@ __ps1_prompt() {
       :command)       echo -n '\#';;
       :prompt)        echo -n '\$';;
       :backslash)     echo -n '\\';;
+      :style)         __ps1_prompt $(__ps1_style "$1");;
       :status)        echo -n '`__ps1_status`';;
       :statusline)    echo -n '`__ps1_status -n`';;
       +*|-*)          echo -n '\['; __ps1_mode "$arg"; echo -n '\]';;
@@ -163,18 +165,20 @@ __ps1_prompt() {
 }
 
 __ps1_style() {
-  style="${PS1_STYLE:-default}"
   properties=("$@" 'default')
   for property in "${properties[@]}"; do
-    __ps1_style_${style}_$property 2>/dev/null && return
-    __ps1_style_default_$property 2>/dev/null
+    for style in "${PS1_STYLES[@]}"; do
+      __ps1_style_${style}_${property} 2>/dev/null && return
+    done
   done
 }
 
 __ps1_status() {
   export PS1_LAST_ERR="$?"
+  local newline=0; if [[ "$1" == '-n' ]]; then newline=1; shift; fi
+  local modules=("${PS1_MODULES[@]}"); [[ "$1" ]] && modules=("$@")
   local status_items=0
-  for module in "${PS1_MODULES[@]}"; do
+  for module in "${modules[@]}"; do
     local module_out="$(__ps1_module_${module}_output)"
     [[ "$module_out" == '' ]] && continue
     (( status_items++ )) && __ps1_ansi $(__ps1_style block_pad)
@@ -188,35 +192,75 @@ __ps1_status() {
       :reset
   done
   unset PS1_LAST_ERR
-  if [[ "$status_items" > 0 && "$1" == '-n' ]]; then
+  if [[ "$status_items" > 0 && "$newline" == 1 ]]; then
     __ps1_ansi :eol
   fi
+  return $PS1_LAST_ERR
 }
 
-export PS1_ROOT=$(cd "$(dirname '${BASH_SOURCE[@]}')" &>/dev/null && pwd)
-
-#for module in "$@"; do
-#  source $PS1_ROOT/module/$module.sh
-#  (( "$?" )) || PS1_MODULES+=("$module")
-#done
-#for module in "${PS1_MODULES[@]}"; do
-#  echo "MODULE: $module"
-#done
+# Process --key=val args into --key val, un-bundle single-letter flags
+args=(); for (( i=1; i<=$#; i++ )); do a="${@:$i:1}"
+  [[   "$a" == '--'       ]] && args+=("${@:$i}")            && break
+  [[   "$a" == '--'*'='*  ]] && args+=("${a%%=*}" "${a#*=}") && continue
+  [[ ! "$a" =~ ^-([^-]+)$ ]] && args+=("$a")                 && continue
+  args+=( $( for (( x=1; x<${#a}; x++ )); do echo "-${a:$x:1}"; done ) )
+done
+set -- "${arg[@]}"
 
 export PS1_MODULES=()
-for module_path in $PS1_ROOT/module/*.sh; do
-  [[ -x $module_path ]] || continue
-  if source $module_path; then
+export PS1_STYLES=('user' 'default')
+export PS1_ROOT=$(cd "$(dirname '${BASH_SOURCE[@]}')" &>/dev/null && pwd)
+all_modules=0
+while (( "$#" > 0 )); do arg="$1"; shift; case "$arg" in
+  -m|--module)      PS1_MODULES+=("$1"); shift ;;
+  -a|--all-modules) all_modules=1 ;;
+  -s|--style)       PS1_STYLES=("$1" "${PS1_STYLES[@]}"); shift ;;
+  -u|--user-style)  str="__ps1_style_user_${1%%=*}() { echo -n ${1#*=}; }"
+                    __ps1_debug "$str"
+                    eval "$str";;
+  -r|--root)        PS1_ROOT="$1"; shift ;;
+  -p|--prompt)      [[ "$PS1_ORIG" == '' ]] && export PS1_ORIG="$PS1"
+                    export PS1="$(__ps1_prompt $1 :title)"
+                    shift;;
+  -d|--debug)       PS1_DEBUG=1 ;;
+  -t|--title)       PS1_TITLE_FORMAT="$1"; shift ;;
+  -n|--no-title)    PS1_NO_TITLE=1 ;;
+esac; done
+
+__ps1_debug "PS1_ROOT: $PS1_ROOT"
+
+if (( all_modules )); then
+  PS1_MODULES=()
+  for module_path in $PS1_ROOT/module/*.sh; do
     PS1_MODULES+=("$(basename -s .sh $module_path)")
+  done
+fi
+
+err=0
+for module in "${PS1_MODULES[@]}"; do
+  __ps1_debug "PS1_MODULE: $module"
+  if ! source ${PS1_ROOT}/module/${module}.sh; then
+    echo "Unable to open $0 module ${PS1_ROOT}/modules/${module}.sh" >&2
+    (( err++ ))
+  fi
+done
+for style in "${PS1_STYLES[@]}"; do
+  [[ "$style" == 'user' ]] && continue
+  __ps1_debug "PS1_STYLE: $style"
+  if ! source ${PS1_ROOT}/style/${style}.sh; then
+    echo "Unable to open $0 style ${PS1_ROOT}/style/${style}.sh" >&2
+    (( err++ ))
   fi
 done
 
-for style_path in $PS1_ROOT/style/*.sh; do
-  source $style_path
-done
-export PS1_STYLE=solid
-
 if [[ "$PS1_ORIG" == '' ]]; then
-  default_prompt
+  # Set default prompt if we haven't set one already
+  export PS1="$(
+    __ps1_prompt :statusline \
+      :user :reset '@' :host ' ' :dir :eol \
+      :prompt ' ' :title :reset
+  )"
 fi
+
+return $err
 
--- style/default.sh
+++ style/default.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
+__ps1_style_default_default()     { echo -n; }
 __ps1_style_default_user()        { echo -n :fg cyan; }
 __ps1_style_default_host()        { echo -n :fg magenta; }
 __ps1_style_default_dir()         { echo -n :fg green; }
 __ps1_style_default_time()        { echo -n :fg blue +reverse; }
 __ps1_style_default_date()        { echo -n :fg blue +reverse; }
-__ps1_style_default_block_start() { echo -n '[' :space; }
-__ps1_style_default_block_end()   { echo -n :space ']'; }
-__ps1_style_default_block_pad()   { echo -n :space; }
+
+__ps1_style_default_block_start() {
+  if __ps1_unicode; then
+    echo -n +reverse "$(printf '\u258F')"
+  else
+    echo -n +reverse :space
+  fi
+}
+
+__ps1_style_default_block_end() {
+  if __ps1_unicode; then
+    echo -n "$(printf '\u2595')"
+  else
+    echo -n :space
+  fi
+}
+
+__ps1_style_default_block_pad() {
+  if ! __ps1_unicode; then
+    echo -n :space
+  fi
+}
 
