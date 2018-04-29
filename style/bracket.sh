1d42f19f0a3cd9956c164ae174c4b15a2533feba
Kilna <kilna@kilna.com>
2018-04-28 22:28:56 -0700

    Initial development

--- style/bracket.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-__ps1_style_bracket_block_start() { echo -n '[ '; }
-__ps1_style_bracket_block_end()   { echo -n ' ]'; }
-__ps1_style_bracket_block_pad()   { echo -n ' '; }
-
--- style/minimal.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-__ps1_style_minimal_block_start() { echo -n; }
-__ps1_style_minimal_block_end()   { echo -n; }
-__ps1_style_minimal_block_pad()   { echo -n :space; }
-
