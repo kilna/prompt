cf51ef80e33c86c167c77e7bdb6053702581346a
Kilna <kilna@kilna.com>
2018-04-30 01:19:47 -0700

    Initial development

--- testparse.sh
+++ testparse.sh
@@ -6,6 +6,7 @@ lastindex()  { x="${1%$2*}";  [[ "$x" = "$1" ]] || echo $(( ${#x} + 1 )); }
 chunk() {
   str="$1"
   #echo "$str"
+  ar=()
   while [[ "$str" != '' ]]; do
     idx1=$( firstindex "$str" '{' )
     idx2=$( firstindex "$str" '}' )
@@ -13,26 +14,32 @@ chunk() {
       substr="${str:0:(( idx2 ))}"
       idx1=$( lastindex "$substr" '{' )
       if [[ "$idx1" == '' ]]; then
-        echo -n "'"${substr//\'/\'\\\'\'}"' "
+        # Non-tag chunk (no matching beginning '{')
+        ar_idx=$(( "${#ar[@]}" - 1 ))
+        echo "ar_idx: $ar_idx"
+        if [[ (( ar_idx < 0 )) ]]; then
+          ar+=("$substr")
+        elif [[ "${ar[$ar_idx]}" == '{'*'}' ]]; then
+          ar+=("$substr")
+        else
+          ar[$ar_idx]+="$substr" 
+        fi
       else
         if (( idx1 != 0 )); then
-          chunk="${str:0:(( idx1 - 1 ))}"
-          echo -n "'"${chunk//\'/\'\\\'\'}"' "
+          ar+=("${str:0:(( idx1 - 1 ))}") # Non-tag chunk
         fi
-        chunk="${str:(( idx1 - 1 )):(( idx2 - idx1 + 1 ))}"
-        echo -n "'"${chunk//\'/\'\\\'\'}"' "
+        ar+=("${str:(( idx1 - 1 )):(( idx2 - idx1 + 1 ))}") # Tag chunk
       fi
-      str="${str:(( idx2 ))}"
+      str="${str:$idx2}"
     else
-      echo -n "'"${str//\'/\'\\\'\'}"' "
+      ar+=("$str") # Last non-tag chunk
       str=''
     fi
   done
+  for x in "${ar[@]}"; do
+    echo "$x"
+  done
 }
 
-eval 'ar=('$(chunk "as}df{foo}as}df 'foo' bar 'baz'{bar}{{/foo}asdf{}asdf's")')'
-
-for x in "${ar[@]}"; do
-  echo "$x"
-done
+chunk "as}df{foo}as}df 'foo' bar 'baz'{bar}{{/foo}asdf{}asdf's"
 
